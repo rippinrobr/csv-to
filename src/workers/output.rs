d0f242600e6e709be9a94f0c42ec95c0c1d37982
Rob Rowe <rrowe@foodlogiq.com>
2018-06-20 20:20:09 -0400

    CodeGen now generaets the main.rs file which runs a simple http server that responds to the / route.  Added code to expose the db_actor module, and the models module. Re-organized the writing of files so that the write functions are in the same module as what they write out.

--- src/workers/output.rs
+++ src/workers/output.rs
@@ -1,6 +1,10 @@
-use std::fs::File;
-use std::io::Error;
-use std::io::prelude::*;
+// TODO: This will become the struct that lays out where the results of the
+// data is stored, typically it will be a database.  Maybe this should have
+// a vec of output types as a field so I can havae SQLite, Postgress, MySQL, 
+// etc...
+// use std::fs::File;
+// use std::io::Error;
+// use std::io::prelude::*;
 
 #[derive(Debug)]
 pub struct Output {
@@ -13,31 +17,6 @@ impl Output {
         Output{
             src_directory: src_directory,
             sql_directory: sql_directory,
-        }
-    }
-
-    pub fn write_code_to_file(&self, dir_path: &str, file_name: &str, code: String) -> Result<String, Error> {
-
-        match File::create(format!("{}/{}.rs", dir_path, &file_name).to_lowercase()) {
-            Ok(mut file) => {
-                match file.write_all(&code.into_bytes()) {
-                    Ok(_) => Ok(file_name.to_string()),
-                    Err(e) => Err(e)
-                }
-            },
-            Err(e) => Err(e)
-        }
-    }
-
-    pub fn write_sql_to_file(&self, file_name: &str, sql_str: String) -> Result<String, Error> {
-        match File::create(format!("{}/{}.sql", &self.sql_directory, &file_name).to_lowercase()) {
-            Ok(mut file) => {
-                match file.write_all(&sql_str.into_bytes()) {
-                    Ok(_) => Ok(file_name.to_string()),
-                    Err(e) => Err(e)
-                }
-            },
-            Err(e) => Err(e)
         }
     }
 }
