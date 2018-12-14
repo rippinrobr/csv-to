203aaa2e684aaa7076ee1f4e0723a92cb786b0fa
Rob Rowe <rrowe@foodlogiq.com>
2018-12-14 08:40:17 -0500

    debugging the run-kcov script

--- .scripts/run-kcov.sh
+++ .scripts/run-kcov.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 binaries=`ls -l ./target/tests/debug/csv_to-* | grep -v dSYM | grep -v "\.d" | awk '{print $NF}'`
+echo "binaries: ${binaries}"
 if [[ ! -d "./target/cov" ]]; then
   mkdir -p ./target/cov
 fi
 
+ls -lt ./target/cov
 for test_bin in $binaries
 do
+    echo "About to test ${test_bin}"
     kcov ./target/cov/ ${test_bin}
     ls -lt ./target/cov
 done
